b0561b79d0264fd9c29cc71ff3d813b897f9017a
Amy Ji <amy.ji.1123@gmail.com>
2022-08-22 20:57:26 -0400

    add event button is navigating to the addevent page

--- navigation/screens/AddEvent.js
+++ navigation/screens/AddEvent.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, Text, View, StyleSheet, ScrollView, SafeAreaView, Pressable
+import { View, TextInput
 } from 'react-native';
 
 export default function AddEvent() {
     return (
-        <TextInput/>
+        <View>
+            <TextInput/>
+        </View>
     )
 }
