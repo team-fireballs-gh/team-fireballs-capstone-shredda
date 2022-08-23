1cb911bd7ecee922d33638b526478b0dceb3d1a4
Donna-Sayos <Donnasayos@gmail.com>
2022-08-23 11:53:07 -0400

    swiper working

--- navigation/screens/AddUsers.js
+++ navigation/screens/AddUsers.js
@@ -1,7 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import react from "react";
-import AntIcon from "react-native-vector-icons/AntDesign";
-
+import React, { useRef } from "react";
+import { AntDesign, Ionicons } from "react-native-vector-icons";
 import {
   StyleSheet,
   Text,
@@ -9,116 +7,231 @@ import {
   SafeAreaView,
   Image,
   TouchableOpacity,
-  Button,
-  Alert,
-  Platform,
-  Dimensions,
 } from "react-native";
-import { useDimensions } from "@react-native-community/hooks";
+import Swiper from "react-native-deck-swiper";
+
+const Dummy_Data = [
+  {
+    displayName: "Sommer Ray",
+    job: "Influencer",
+    photoURL:
+      "https://www.lifeandstylemag.com/wp-content/uploads/2018/12/058703039872_10-e1545171758516.jpg?resize=940%2C529&quality=86&strip=all",
+    age: 25,
+    uid: 13,
+  },
+  {
+    displayName: "Scarlett Johansson",
+    job: "Actress",
+    photoURL:
+      "https://m.media-amazon.com/images/M/MV5BMTM3OTUwMDYwNl5BMl5BanBnXkFtZTcwNTUyNzc3Nw@@._V1_.jpg",
+    age: 37,
+    uid: 15,
+  },
+  {
+    displayName: "Boberty Ton",
+    job: "Instructor at Fullstack Academy",
+    photoURL:
+      "https://ca.slack-edge.com/T024FPYBQ-U033R8PS4MN-a1075cbb5839-512",
+    age: 25,
+    uid: 11,
+  },
+  {
+    displayName: "Angelina Jolie",
+    job: "Actress",
+    photoURL:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2005-1565016334.jpg",
+    age: 47,
+    uid: 17,
+  },
+  {
+    displayName: "Cara Delevingne",
+    job: "Supermodel",
+    photoURL:
+      "https://www.suntiros.com/wp-content/uploads/2021/02/Cara-Delevingne-Makeup-Pictures.jpg",
+    age: 30,
+    uid: 19,
+  },
+  {
+    displayName: "Colson Baker",
+    job: "Rapper",
+    photoURL:
+      "https://www.popkiller.pl/sites/default/files/images/mgk-2019.jpg",
+    age: 32,
+    uid: 21,
+  },
+  {
+    displayName: "Alec Friedman",
+    job: "Lead Web Development Instructor at Fullstack Academy",
+    photoURL:
+      "https://ca.slack-edge.com/T024FPYBQ-U033D99P960-2d5d5c907d83-512",
+    age: "👀",
+    uid: 23,
+  },
+];
 
 export default function AddUsers({ navigation }) {
-  // console.log(Dimensions.get("screen"));
-  // console.log(useDimensions());
-
+  const swipeRef = useRef(null);
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View style={styles.imageContainer}>
-        <View>
-          <Image
-            style={{
-              height: 130,
-              width: "100%",
-              borderTopLeftRadius: 20,
-              borderTopRightRadius: 20,
-            }}
-            source={{
-              uri: "https://media.istockphoto.com/photos/forest-wooden-table-background-summer-sunny-meadow-with-green-grass-picture-id1353553203?b=1&k=20&m=1353553203&s=170667a&w=0&h=QTyTGI9tWQluIlkmwW0s7Q4z7R_IT8egpzzHjW3cSas=",
-            }}
-          />
-        </View>
-
-        <View style={styles.profilePic}>
-          <Image
-            style={{ height: "100%", width: "100%", borderRadius: 50 }}
-            source={{
-              uri: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/41822045014839.5824bf369f54b.jpg",
-            }}
-          />
-        </View>
-
-        <View style={styles.textContainer}>
-          <Text style={styles.text}>Shauna, The Queen</Text>
-
-          <Text style={{ color: "#9597A1" }}>
-            Pronouns, Interests, Tag line/ first impression
-          </Text>
-        </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.chatBubble}>
+        <TouchableOpacity onPress={() => navigation.navigate("Chats")}>
+          <Ionicons name="chatbubbles-sharp" size={45} color="#fea7a5" />
+        </TouchableOpacity>
       </View>
+      {/* User Cards */}
+      <View style={styles.swiperContainer}>
+        <Swiper
+          ref={swipeRef}
+          containerStyle={styles.cardContainer}
+          cards={Dummy_Data}
+          stackSize={5} // so you can actually see 5 cards stacked together;
+          cardIndex={0} // so the card will always start at the very first person on the list;
+          animateCardOpacity // when you're swiping, the card becomes transparent;
+          verticalSwipe={false} // so you only swipe left and right... NOT up and down;
+          // we can set verticalSwipe to TRUE for a "super like feature?";
+          onSwipedLeft={() => console.log("swiped left")} // you can a callback when swiping left || right;
+          onSwipedRight={() => console.log("swiped right")}
+          overlayLabels={{
+            // to create labels when you're swiping left or right;
+            left: {
+              title: "🤔",
+              style: {
+                label: {
+                  textAlign: "right",
+                  color: "red",
+                },
+              },
+            },
+            right: {
+              title: "MATCH",
+              style: {
+                label: {
+                  color: "#00F763",
+                },
+              },
+            },
+          }}
+          renderCard={(
+            card // this is where you map through the cards array;
+          ) => (
+            <View key={card.uid} style={[styles.eachCard, styles.shadow]}>
+              <Image
+                style={styles.imageStyle}
+                source={{ uri: card.photoURL }}
+              />
+
+              <View style={styles.cardInfo}>
+                <View>
+                  <Text style={{ fontWeight: "bold", fontSize: 20 }}>
+                    {card.displayName}
+                  </Text>
+                  <Text style={{ flexWrap: "wrap", width: "60%" }}>
+                    {card.job}
+                  </Text>
+                </View>
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                    fontSize: 25,
+                    position: "absolute",
+                    left: "90%",
+                  }}
+                >
+                  {card.age}
+                </Text>
+              </View>
+            </View>
+          )}
+        />
+      </View>
+
       <View style={styles.footer}>
-        <View style={styles.circle}>
-          <AntIcon name="close" size={35} color="#FFC1CB" />
-        </View>
-        <View style={styles.circle}>
-          <AntIcon name="hearto" size={35} color="#EB5559" />
-        </View>
+        <TouchableOpacity
+          style={[styles.circle, { backgroundColor: "#FFCCCC" }]}
+          onPress={() => swipeRef.current.swipeLeft()} // comes from useRef() which contains a .current property which can hold a mutable value (Objects whose value can change);
+        >
+          <AntDesign name="close" size={30} color="#FF3D66" />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[styles.circle, { backgroundColor: "#C2FFD6" }]}
+          onPress={() => swipeRef.current.swipeRight()}
+        >
+          <AntDesign name="heart" size={30} color="#00AD5F" />
+        </TouchableOpacity>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "beige",
-    justifyContent: "space-evenly",
-    // alignItems: 'center',
-
+    backgroundColor: "white",
     // padding: Platform.OS === "ios" ? StatusBar.currentHeight : 0,
   },
-  imageContainer: {
-    height: 430,
-    width: 345,
-    backgroundColor: "white",
-    borderRadius: 21,
-    elevation: 4,
-    marginLeft: 23,
+  chatBubble: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  swiperContainer: {
+    flex: 1,
+  },
+  cardContainer: {
+    backgroundColor: "transparent",
+  },
+  eachCard: {
+    position: "relative",
+    top: -50,
+    backgroundColor: "beige",
+    height: "70%",
+    borderRadius: 20,
+  },
+  imageStyle: {
+    height: "100%",
+    width: "100%",
+    borderRadius: 20,
+  },
+  cardInfo: {
+    position: "absolute",
+    backgroundColor: "#FFD9C7",
+    bottom: 0,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    paddingLeft: 15,
+    paddingRight: 10,
+    paddingTop: 10,
+    paddingBottom: 20,
+  },
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.45,
+    elevation: 2,
   },
   footer: {
     flexDirection: "row",
     justifyContent: "space-evenly",
+    paddingBottom: 15,
   },
   circle: {
-    width: 64,
-    height: 64,
+    width: 60,
+    height: 60,
     borderRadius: 32,
     padding: 12,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "white",
     shadowColor: "gray",
     shadowOffset: { width: 1, height: 1 },
     shadowOpacity: 0.2,
     shadowRadius: 2,
-  },
-  profilePic: {
-    height: 135,
-    width: 140,
-    alignSelf: "center",
-    position: "absolute",
-    top: 75,
-  },
-  textContainer: {
-    height: 45,
-    width: "100%",
-    position: "absolute",
-    top: 200,
-    alignItems: "center",
-  },
-  text: {
-    alignSelf: "center",
-    fontSize: 15,
-    lineHeight: 48,
-    fontWeight: "bold",
   },
 });
