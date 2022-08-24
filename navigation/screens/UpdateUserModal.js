3211dc2bd94c5bf3e0f4081b6bef6c279cdaabfd
Donna-Sayos <Donnasayos@gmail.com>
2022-08-24 15:50:36 -0400

    separated functions

--- navigation/screens/UpdateUserModal.js
+++ navigation/screens/UpdateUserModal.js
@@ -6,8 +6,6 @@ import {
   SafeAreaView,
   Image,
   ScrollView,
-  TextInput,
-  Dimensions,
   Platform,
   Alert,
   Button,
@@ -17,60 +15,7 @@ import {
 import * as ImagePicker from "expo-image-picker";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/db";
-
-const INTERESTS = [
-  "Food",
-  "Music",
-  "Travel",
-  "Sports",
-  "Television",
-  "Camping",
-  "Exercise",
-  "Dancing",
-  "Art",
-  "Cooking",
-  "Video Games",
-];
-const WIDTH = Dimensions.get("screen").width;
-const HEIGHT = Dimensions.get("screen").height;
-
-const InterestPicker = (props) => { // for interests selector;
-  const onPressInterest = (option) => {
-    props.changeModalVisibility(false);
-    props.setData(option);
-  };
-  const option = INTERESTS.map((interest, i) => {
-    return (
-      <TouchableOpacity
-        style={{ alignItems: "flex-start" }}
-        key={i}
-        onPress={() => onPressInterest(interest)}
-      >
-        <Text style={{ margin: 20, fontSize: 20, fontWeight: "bold" }}>
-          {interest}
-        </Text>
-      </TouchableOpacity>
-    );
-  });
-
-  return (
-    <TouchableOpacity
-      onPress={() => props.changeModalVisibility(false)}
-      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-    >
-      <View
-        style={{
-          backgroundColor: "#FFD6A1",
-          borderRadius: 10,
-          width: WIDTH - 180,
-          height: HEIGHT - 300,
-        }}
-      >
-        <ScrollView>{option}</ScrollView>
-      </View>
-    </TouchableOpacity>
-  );
-};
+import { InterestPicker } from "./helper/InterestPicker";
 
 export default function Chats({ navigation }) {
   const [user] = useAuthState(auth);
