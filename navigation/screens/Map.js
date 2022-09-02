c61b16232d98ed599e6bf880d4351fa9d3922afc
Amy Ji <amy.ji.1123@gmail.com>
2022-09-02 01:04:44 -0400

    marker

--- navigation/screens/Map.js
+++ navigation/screens/Map.js
@@ -5,7 +5,15 @@ import { StyleSheet, Text, View, Dimensions } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+        <MapView style={styles.map} 
+            initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }}> 
+            <Marker coordinate={{latitude: 37.78825, longitude: -122.4324}}></Marker>
+        </MapView>
     </View>
   );
 }
