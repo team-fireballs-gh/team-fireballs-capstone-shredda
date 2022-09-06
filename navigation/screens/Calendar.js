01f34b9f04a68271762dcf2585fd2291c33d132b
Amy Ji <amy.ji.1123@gmail.com>
2022-09-06 17:26:31 -0400

    calendar

--- navigation/screens/Calendar.js
+++ navigation/screens/Calendar.js
@@ -1,31 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from 'react-native';
 import { CalendarList } from 'react-native-calendars';
-import { useSelector, useDispatch } from "react-redux"; 
-import { getAllEvents } from "../../redux/reducers/events/eventsReducer";
+
 
 export default function Calendar({ navigation }) {
-  let events = useSelector((state) => state.events);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getAllEvents());
-  }, []);
-  
   return (
     <View style={styles.container}>
       <CalendarList
-      onDayPress={day => {
-        console.log('selected day', day);
-      }}
-          
-      markedDates={{
-        today : {selected: true, marked: true, selectedColor: 'blue'},
-        '2022-09-17': {marked: true},
-        '2022-09-18': {marked: true, dotColor: 'red', activeOpacity: 0},
-        '2022-09-19': {disabled: true, disableTouchEvent: true}
-      }}
+        markingType={'period'}
+        markedDates={{
+          '2022-09-15': {marked: true, dotColor: '#50cebb'},
+          '2022-09-16': {marked: true, dotColor: '#50cebb'},
+          '2022-09-23': {startingDay: true, color: '#50cebb', textColor: 'white', marked: true, dotColor: 'white'},
+          '2022-09-24': {color: '#70d7c7', textColor: 'white'},
+          '2022-09-25': {endingDay: true, color: '#50cebb', textColor: 'white'}
+        }}
+      
+      
       />
+
     </View>
   );
 }
